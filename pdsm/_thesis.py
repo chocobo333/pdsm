1afd98411cbc7b1892b19dea1f8feff430d12a5c
chocobo333 <chocobo1355@gmail.com>
2020-12-18 01:34:10 +0000

    make it better

--- pdsm/_thesis.py
+++ pdsm/_thesis.py
@@ -1,4 +1,15 @@
 
-def run():
-    print("in thesis")
+import numpy as np
+import sporco.util as util
 
+import pdsm.pdsm as pdsm
+
+
+def run():
+    img = util.ExampleImages().image('kodim23.png', scaled=True, gray=True, idxexp=np.s_[160:416, 60:316])
+    npd = 16
+    fltlmbd = 10
+    sl, sh = util.tikhonov_filter(img, fltlmbd, npd)
+    D = util.convdicts()['G:12x12x36']
+
+    pdsm.ConvBPDNL1L1(D, sh)
--- pdsm/pdsm.py
+++ pdsm/pdsm.py
@@ -1,20 +1,29 @@
 
-from sporco import common
-from sporco import admm
-from sporco.admm import cbpdn
+from __future__ import annotations
+
+import numpy as np
+# import numpy.typing as npt
+import sporco.cnvrep as cr
+import sporco.util as util
+import sporco as sp
+import sporco.common as sc
+
 
 from dataclasses import dataclass
 
+from typing import Any, Union, Literal
 
-class Pdsm(common.IterativeSolver):
+
+class Pdsm(sc.IterativeSolver):
     r"""Base class for Primal-Dual Splitting Method (PDSM).
 
     Solve an optimisation problem of the form
 
     .. math::
-       \mathrm{argmin}_{\mathbf{x},\mathbf{y}} \;
-       f(\mathbf{x}) + g(\mathbf{y}) \;\mathrm{such\;that}\;
-       A\mathbf{x} + B\mathbf{y} = \mathbf{c} \;\;.
+        \newcommand{\argmin}{\mathop{\rm arg~min}\limits}
+        \newcommand{\prox}{\mathop{\mathrm{prox}}\nolimits}
+
+        \argmin_{\mathbf{x}} F(\mathbf{x}) + G(\mathbf{x}) + H(\mathbf{L}\mathbf{x})
 
     This class is intended to be a base class of other classes that
     specialise to specific optimisation problems.
@@ -22,7 +31,49 @@ class Pdsm(common.IterativeSolver):
 
     @dataclass
     class Option:
-        rho: int
+        rho_p: float
+        rho_d: float
+        theta: float
+
+    timer: util.Timer
+    opt: Pdsm.Option
+
+    def __new__(cls, *args: Any, **kwargs: Any) -> Pdsm:
+        """Create an Pdsm object and start its initialisation timer."""
+
+        instance = super(Pdsm, cls).__new__(cls)
+        instance.timer = util.Timer(['init', 'solve', 'solve_wo_func', 'solve_wo_rsdl'])
+        instance.timer.start('init')
+        return instance
+
+    def step(self):
+        r"""Evaluate one step of below iteration
+
+        .. math::
+            \tilde{\mathbf{x}}_{n+1} &= \prox_{\tau G}(\mathbf{x}_n - \tau\nabla F(\mathbf{x}_n) - \tau L^{\dagger}\mathbf{y}_n)\\
+            \tilde{\mathbf{y}}_{n+1} &= \prox_{\sigma H^{*}}(\mathbf{y}_n - \sigma L(2\tilde{\mathbf{x}}_{n+1} - \mathbf{x}_n)\\
+            (\mathbf{x}_{n+1}, \mathbf{y}_{n+1}) &= \rho_n(\tilde{\mathbf{x}}_{n+1}, \tilde{\mathbf{y}}_{n+1}) + (1 - \rho_n)(\mathbf{x}_{n}, \mathbf{y}_{n})
+
+        """
+        self._primal_step()
+        self._dual_step()
+
+    def primal_step(self):
+        r"""
+
+        .. math::
+            \tilde{\mathbf{x}}_{n+1} = \prox_{\tau G}(\mathbf{x}_n - \tau\nabla F(\mathbf{x}_n) - \tau L^{\dagger}\mathbf{y}_n)
+        """
+        pass
+
+    def dual_step(self):
+        r"""
+
+        .. math::
+            \tilde{\mathbf{y}}_{n+1} = \prox_{\sigma H^{*}}(\mathbf{y}_n - \sigma L(2\tilde{\mathbf{x}}_{n+1} - \mathbf{x}_n)
+        """
+        pass
+
 
 class GenericConvBPDN(Pdsm):
 
@@ -30,4 +81,48 @@ class GenericConvBPDN(Pdsm):
     class Option(Pdsm.Option):
         pass
 
-# class ConvBPDNL1L1
+
+class ConvBPDNL1(GenericConvBPDN):
+    @dataclass
+    class Option(GenericConvBPDN.Option):
+        h: None
+
+
+class ConvBPDNL1L1(GenericConvBPDN):
+    r"""
+
+    Solve the optimization problem
+
+    .. math::
+        \argmin_{\mathbf{x}} \| \mathbf{D}\mathbf{x} - \mathbf{s} \|_{1} + \| \mathbf{x} \|_{1}
+    """
+
+    cri: cr.CSC_ConvRepIndexing
+
+    @dataclass
+    class Option(GenericConvBPDN.Option):
+        pass
+
+    def __init__(self, D: np.ndarray, S: np.ndarray, dimK: Union[Literal[1, 2], None, int] = None, dimN: int = 2) -> None:
+        # Infer problem dimensions and set relevant attributes of self
+        self.cri = cr.CSC_ConvRepIndexing(D, S, dimK=dimK, dimN=dimN)
+
+    def primal_step(self):
+        r"""
+
+        .. math::
+            \tilde{\mathbf{x}}_{n+1} &= \prox_{\tau \|\bullet\|_{1}}(\mathbf{x}_n - \tau \mathbf{D}^{\dagger}\mathbf{y}_n)\\
+            &= \mathcal{S}_{\tau}(\mathbf{x}_n - \tau \mathbf{D}^{\dagger}\mathbf{y}_n)\\
+            &= \mathrm{sign}(\mathbf{x}_n - \tau \mathbf{D}^{\dagger}\mathbf{y}_n) \odot \max(0, |\mathbf{x}_n - \tau \mathbf{D}^{\dagger}\mathbf{y}_n| - \tau)
+        """
+        pass
+
+    def dual_step(self):
+        r"""
+
+        .. math::
+            \tilde{\mathbf{y}}_{n+1} &= \prox_{\sigma \|\bullet\|_{1}^{*}}(\mathbf{y}_n - \sigma \mathbf{D}(2\tilde{\mathbf{x}}_{n+1} - \mathbf{x}_n))\\
+            &= \mathbf{y}_n - \sigma \mathbf{D}(2\tilde{\mathbf{x}}_{n+1} - \mathbf{x}_n) - \sigma\prox_{\frac{1}{\sigma}\|\bullet\|_{1}}(\frac{\mathbf{y}_n}{\sigma} - \mathbf{D}(2\tilde{\mathbf{x}}_{n+1} - \mathbf{x}_n))\\
+            &= \mathbf{y}_n - \sigma \mathbf{D}(2\tilde{\mathbf{x}}_{n+1} - \mathbf{x}_n) - \sigma\mathcal{S}_{\frac{1}{\sigma}}(\frac{\mathbf{y}_n}{\sigma} - \mathbf{D}(2\tilde{\mathbf{x}}_{n+1} - \mathbf{x}_n))\\
+        """
+        pass
